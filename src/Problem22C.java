f53eb4e95c6291e5a7be18127e977ddf7abadcd4
Nixon Pastor <nixonpastor@Nixons-MacBook-Pro.local>
2020-02-28 00:09:57 -0600

    Initial commit

---
+++ src/Problem22C.java
@@ -0,0 +1,101 @@
+import java.security.SecureRandom;
+
+public class Problem22C {
+    /*
+    Nixon Pastor
+    Chapter#7 Problem 22C
+    Knight's Tour
+    */
+    public static SecureRandom secureRandom = new SecureRandom();
+    public static int[][] board=new int[8][8];
+    public static int[] horizontal = {2,1,-1,-2,-2,-1,1,2};
+    public static int[] vertical = {-1,-2,-2,-1,1,2,2,1};
+    public static int currentRow;
+    public static int currentColumn;
+    public static int moveNumber = 1;
+    public static int[][] accessibility = {
+            {2,3,4,4,4,4,3,2},
+            {3,4,6,6,6,6,4,3},
+            {4,6,8,8,8,8,6,4},
+            {4,6,8,8,8,8,6,4},
+            {4,6,8,8,8,8,6,4},
+            {4,6,8,8,8,8,6,4},
+            {3,4,6,6,6,6,4,3},
+            {2,3,4,4,4,4,3,2}};
+
+
+    public static void main(String[] args) {
+
+        currentRow = secureRandom.nextInt(8);
+        currentColumn = secureRandom.nextInt(8);
+
+        board[currentRow][currentColumn] = moveNumber;
+
+        int tempRow;
+        int tempColumn;
+        int minimumRow= 0;
+        int minimumColumn = 0;
+        int accessNum;
+        int accessBoard = 9;
+
+        boolean gameOver = false;
+        boolean validMove;
+
+        while ( !gameOver )
+        {
+            accessNum = accessBoard;
+
+            for ( int moveNu = 0; moveNu < 8; moveNu++ )
+            {
+                tempRow = currentRow + vertical[ moveNu ];
+                tempColumn = currentColumn + horizontal[ moveNu ];
+                validMove = ValidMove( tempRow, tempColumn );
+
+                if(validMove) {
+                    if(accessibility[tempRow][tempColumn] < accessNum) {
+                        accessNum = accessibility[tempRow][tempColumn];
+                        minimumRow = tempRow;
+                        minimumColumn = tempColumn;
+                    }
+                    --accessibility[tempRow][tempColumn];
+                }
+            }
+
+            if(accessNum == accessBoard) {
+                gameOver = true;
+            }
+            else {
+                currentRow = minimumRow;
+                currentColumn = minimumColumn;
+                board[currentRow][currentColumn] = ++moveNumber;
+            }
+        }
+
+        System.out.printf( "The tour had %d moves.\n", moveNumber);
+
+        printBoard();
+    }
+
+
+    public static boolean ValidMove( int row, int column )
+    {
+        //if in bound and unvisited
+        if(row>=0 & row < 8 && column >= 0 && column < 8 && board[row][column] == 0){
+            return true;
+        }
+        else
+            return false;
+
+    }
+
+    public static void printBoard()
+    {
+        System.out.println();
+        for( int row = 0; row < board.length; row++ )
+        {
+            for(int column = 0; column < board.length; column++ )
+                System.out.printf(" %2d ", board[row][column] );
+            System.out.println();
+        }
+    }
+}
